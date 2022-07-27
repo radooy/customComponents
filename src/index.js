1f48e8d098d52b78e949813a3d9814f97a766244
Radostin Kirilov <radostin.kirilov@elando.bg>
2022-07-27 16:23:53 +0300

    Remove strict mode so marker gets displayed

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
